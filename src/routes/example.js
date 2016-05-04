b531e068a0cc9da24d71492b5d0624c657c3f153
Cecile Tonglet <cecile.tonglet@gmail.com>
2016-05-04 16:01:00 +0200

    Update custom handler example using async/await

--- src/routes/example.js
+++ src/routes/example.js
@@ -1,10 +1,10 @@
+import { selectQuery } from '../sparql'
 import util from 'util'
+import Boom from 'boom'
 
-if (process.env.MU_APPLICATION_GRAPH === undefined) {
-  throw new Error('missing environment variable MU_APPLICATION_GRAPH')
-}
-
-const graph = process.env.MU_APPLICATION_GRAPH
+const graph = process.env.MU_APPLICATION_GRAPH !== undefined
+  ? process.env.MU_APPLICATION_GRAPH
+  : 'http://mu.semte.ch/application'
 
 const selectExample = {
   method: 'GET',
@@ -37,18 +37,22 @@ const constructExample = {
 const customHandlerExample = {
   method: 'GET',
   path: '/custom',
-  handler: (request, reply) => {
-    const endpoint = request.server.plugins['hapi-sparql'].endpoint
-    endpoint.selectQuery(
-      `SELECT * FROM <${graph}> WHERE {?s ?p ?o}`, (err, result) => {
-        if (err) {
-          throw err
-        }
-        const jsonResult = JSON.parse(result.body)
-        request.server.log('info', 'reply: ' +
-          util.inspect(jsonResult, {colors: true, depth: null}))
-        reply(jsonResult)
-      })
+  handler: async (request, reply) => {
+    let result
+    try {
+      result = await selectQuery(
+        `SELECT * FROM <${graph}> WHERE {?s ?p ?o}`)
+    } catch (e) {
+      return reply(Boom.badImplementation(
+        `Received ${e.result.statusCode} ${e.result.statusMessage}: ` +
+        e.result.body))
+    }
+    const jsonResult = JSON.parse(result.body)
+    request.server.log('info', 'reply: ' +
+      util.inspect(jsonResult, {colors: true, depth: null}))
+    return reply({
+      data: jsonResult
+    })
   }
 }
 
--- test/test.js
+++ test/test.js
@@ -1,7 +1,13 @@
-import {expect} from 'chai'
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
 import Hapi from 'hapi'
+import nock from 'nock'
 import parallel from 'mocha.parallel'
 import routes from '../src/routes'
+import {endpoint} from '../src/sparql'
+
+chai.use(chaiAsPromised)
+const expect = chai.expect
 
 /*
  * NOTE: you should use parallel here and not describe, otherwise the
@@ -12,6 +18,8 @@ import routes from '../src/routes'
  */
 
 parallel('mu-semtech-template', () => {
+  after(nock.cleanAll)
+
   const endpointUrl = 'http://mu.semte.ch/application'
   const request = (method, url, headers, content, callback) => {
     callback(null, {
@@ -58,32 +66,35 @@ parallel('mu-semtech-template', () => {
     })
   })
 
-  it('enables creating custom handler', (done) => {
-    const customRequest = (method, url, headers, content, callback) => {
-      callback(null, {
-        headers: {
-          'content-type': 'application/json'
-        },
-        body: '{"status": "ok"}',
-        statusCode: 200
-      })
-    }
+  it('enables creating custom handler', () => {
+    const scope = nock(endpoint.endpointUrl)
+      .get(/\?query=/)
+      .reply(200, ['some', 'data'])
     const server = new Hapi.Server()
     server.connection({})
-    server.register({
-      ...plugin,
-      options: {
-        ...plugin.options,
-        request: customRequest
-      }
-    }, (err) => {
-      expect(err).to.be.not.ok
-      server.route(routes)
-      server.inject('/custom', (res) => {
-        expect(res.statusCode).to.be.equal(200)
-        expect(res.result.status).to.be.ok
-        done()
+    server.route(routes[2])
+    return expect(server.inject('/custom'))
+      .to.eventually.be.fulfilled
+      .to.eventually.have.deep.property('result.data')
+      .to.eventually.be.equal(['some', 'data'])
+      .then(() => expect(scope.isDone()).to.be.ok)
+  })
+
+  it('enables custom handler to fail sometimes', () => {
+    const scope = nock(endpoint.endpointUrl)
+      .get(/\?query=/)
+      .reply(400, 'private bad request message')
+    const server = new Hapi.Server({debug: {request: []}})
+    server.connection({})
+    server.route(routes[2])
+    return expect(server.inject('/custom'))
+      .to.eventually.be.fulfilled
+      .to.eventually.have.deep.property('result')
+      .to.eventually.be.equal({
+        error: 'Internal Server Error',
+        message: 'An internal server error occurred',
+        statusCode: 500
       })
-    })
+      .then(() => expect(scope.isDone()).to.be.ok)
   })
 })
