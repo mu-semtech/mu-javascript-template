f008aa48bfd64d3994419c4013f860bfb32cba05
Cecile Tonglet <cecile.tonglet@gmail.com>
2016-05-09 11:30:18 +0200

    Implements SPARQL validation

--- src/sparql.js
+++ src/sparql.js
@@ -1,6 +1,34 @@
 import request from 'request'
 import SparqlHttp from 'sparql-http-client'
 
+// grammar from:
+// https://www.w3.org/TR/2013/REC-sparql11-query-20130321/#sparqlGrammar
+const ECHAR = '\\\\[tbnrf\\\\"\']'
+const STRING_LITERAL1 = `'([^'\\\\\\n\\r]|${ECHAR})*'`
+const STRING_LITERAL2 = `"([^"\\\\\\n\\r]|${ECHAR})*"`
+const STRING_LITERAL_LONG1 = `'''(('|'')?([^'\\\\]|${ECHAR}))*'''`
+const STRING_LITERAL_LONG2 = `"""(("|"")?([^"\\\\]|${ECHAR}))*"""`
+// NOTE: the following grammar is incomplete
+const HEX = '[a-fA-F0-9]'
+const PERCENT = `%${HEX}${HEX}`
+const PN_LOCAL_ESC = '\\\\[_~.\\-!$&\'()*\\+,;=/?#@%]'
+const PLX = `${PERCENT}|${PN_LOCAL_ESC}`
+const PN_CHARS_BASE = '[a-zA-Z]'
+const PN_CHARS_U = `${PN_CHARS_BASE}|_`
+const PN_CHARS = `${PN_CHARS_U}|-|[0-9]`
+const PN_PREFIX = `(${PN_CHARS_BASE})((${PN_CHARS}|\\\.)*(${PN_CHARS}))?`
+const PN_LOCAL = `(${PN_CHARS_U}|:|[0-9]|${PLX})((${PN_CHARS}|\\\.|:)*(${PN_CHARS}|:|${PLX}))?`
+const PNAME_NS = `(${PN_PREFIX})?:`
+const PNAME_LN = `(${PNAME_NS})${PN_LOCAL}`
+const VARNAME = `(${PN_CHARS_U}|[0-9])+`
+const VAR1 = `\\?${VARNAME}`
+const VAR2 = `\\$${VARNAME}`
+const IRIREF = '<([^<>{}|^\\\\\\x00-\\x20])*>'
+const String = `${STRING_LITERAL1}|${STRING_LITERAL2}|${STRING_LITERAL_LONG1}|${STRING_LITERAL_LONG2}`
+const Var = `${VAR1}|${VAR2}`
+const PrefixedName = `${PNAME_LN}|${PNAME_NS}`
+const iri = `${IRIREF}|${PNAME_NS}`
+
 const endpointUrl = process.env.MU_SPARQL_ENDPOINT !== undefined
     ? process.env.MU_SPARQL_ENDPOINT
     : 'http://database:8890/sparql'
@@ -41,4 +69,20 @@ export function updateQuery (query, options) {
   return new Promise((resolve, reject) => {
     endpoint.updateQuery(query, requestCallback(resolve, reject), options)
   })
+}
+
+export function isStringEscaped (value) {
+  return !!value.match(new RegExp(`^(${String})$`))
+}
+
+export function isVariable (value) {
+  return !!value.match(new RegExp(`^(${Var})$`))
+}
+
+export function isPrefixedName (value) {
+  return !!value.match(new RegExp(`^(${PrefixedName})$`))
+}
+
+export function isIRI (value) {
+  return !!value.match(new RegExp(`^(${iri})$`))
 }
--- test/sparql.js
+++ test/sparql.js
@@ -2,7 +2,8 @@ import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import nock from 'nock'
 import {
-  endpoint, constructQuery, selectQuery, updateQuery
+  endpoint, constructQuery, selectQuery, updateQuery, isStringEscaped,
+  isVariable, isPrefixedName
 } from '../src/sparql'
 import parallel from 'mocha.parallel'
 import request from 'request'
@@ -75,5 +76,47 @@ parallel('sparql helper module', () => {
       .to.eventually.be.rejected
       .to.eventually.have.deep.property('code', 'ESOCKETTIMEDOUT')
       .then(() => expect(scope.isDone()).to.be.ok)
+  })
+
+  it('validates if SPARQL strings are correctly escaped', () => {
+    expect(isStringEscaped('"well escaped"')).to.be.ok
+    expect(isStringEscaped('\'well escaped\'')).to.be.ok
+    expect(isStringEscaped('"""well escaped"""')).to.be.ok
+    expect(isStringEscaped('\'\'\'well escaped\'\'\'')).to.be.ok
+    expect(isStringEscaped('"harmful"string"')).to.be.not.ok
+    expect(isStringEscaped('\'harmful\'string\'')).to.be.not.ok
+    expect(isStringEscaped('"""harmful"""string"""')).to.be.not.ok
+    expect(isStringEscaped('\'\'\'harmful\'\'\'string\'\'\'')).to.be.not.ok
+    expect(isStringEscaped('"harmful\nstring"')).to.be.not.ok
+    expect(isStringEscaped('\'harmful\\string\'')).to.be.not.ok
+    expect(isStringEscaped('"""well\nescaped"""')).to.be.ok
+    expect(isStringEscaped('"""harmful\\string"""')).to.be.not.ok
+  })
+
+  it('validates SPARQL variables', () => {
+    expect(isVariable('?good_variable')).to.be.ok
+    expect(isVariable('$good_variable')).to.be.ok
+    expect(isVariable('?1')).to.be.ok
+    expect(isVariable('?bad-variable')).to.be.not.ok
+  })
+
+  it('validates SPARQL prefixed names', () => {
+    expect(isPrefixedName(':')).to.be.ok
+    expect(isPrefixedName('valid-pn:')).to.be.ok
+    expect(isPrefixedName('valid_pn:')).to.be.ok
+    expect(isPrefixedName('-invalid-pn:')).to.be.not.ok
+    expect(isPrefixedName('valid.pn:')).to.be.ok
+    expect(isPrefixedName('invalid-pn.:')).to.be.not.ok
+    expect(isPrefixedName(':valid-pn')).to.be.ok
+    expect(isPrefixedName(':-invalid-pn')).to.be.not.ok
+    expect(isPrefixedName(':valid:pn')).to.be.ok
+    expect(isPrefixedName(':valid.pn')).to.be.ok
+    expect(isPrefixedName(':valid_pn')).to.be.ok
+    expect(isPrefixedName(':invalid.pn.')).to.be.not.ok
+    expect(isPrefixedName(':valid.pn:')).to.be.ok
+    expect(isPrefixedName(':valid.pn%ff')).to.be.ok
+    expect(isPrefixedName(':invalid.pn%gg')).to.be.not.ok
+    expect(isPrefixedName(':valid.pn\\$')).to.be.ok
+    expect(isPrefixedName(':valid.pn\\.')).to.be.ok
   })
 })
