d43db31639c26cb9d3753c59f1ca71d048e521af
Erika Pauwels <erika.pauwels@gmail.com>
2016-11-22 22:16:34 +0100

    Add JSONAPI Content-Type handling

--- helpers/mu/server.js
+++ helpers/mu/server.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 var app = express();
 
+// parse JSONAPI content type
+app.use(bodyParser.json( { type: function(req) { return /^application\/vnd\.api\+json/.test(req.get('content-type')); } } ));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// set JSONAPI content type
+app.use('/', function(req, res, next) {
+  res.type('application/vnd.api+json');
+  next();
+});
+
+// start server
 app.listen( 80, function() {
   console.log('Starting server on port 80');
 });
