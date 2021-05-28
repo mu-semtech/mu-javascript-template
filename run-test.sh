56aa73053b60a750babfdc3d048b32adbdfc91c1
Niels Vandekeybus <progster@gmail.com>
2021-05-28 14:19:27 +0200

    use same output dir as production build so mu is ingested correctly

--- run-test.sh
+++ run-test.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # create a copy of app, so we can modify as necessary
-cp -a /app /usr/src/out
+cp -a /app /usr/src/output
 
 # add some required files from the template
-cp -a /usr/src/app/.babelrc.js /usr/src/app/helpers /usr/src/out/
+cp -a /usr/src/app/.babelrc.js /usr/src/app/helpers /usr/src/output/
 # merge package.json files, prefering whatever is specified in the app
-jq -s '.[0] * .[1]' /usr/src/app/package.json  /app/package.json > /usr/src/out/package.json
+jq -s '.[0] * .[1]' /usr/src/app/package.json  /app/package.json > /usr/src/output/package.json
 
 # do things
-cd /usr/src/out
+cd /usr/src/output
 npm install
 npm test
