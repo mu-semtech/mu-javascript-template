2247678ee5428036c24f5090d2b77ce7e623d5db
karel kremer <karel.kremer@redpencil.io>
2024-12-22 15:58:54 +0100

    add install script

--- scripts/install/run.sh
+++ scripts/install/run.sh
@@ -7,42 +7,13 @@ then
     exit 1;
 fi
 
-echo "Removing node_modules from service"
-cd /data/service/
-rm -rf node_modules
+rm -rf /data/service/package-lock.json
+mkdir -p /app
+# install script expects service package.json to be in /app
+cp /data/service/package.json /app/package.json
+docker-rsync /data/service/ /usr/src/app/app
+cd /usr/src/app
+NODE_ENV=development ./install-dependencies.sh
 
-echo "Running template npm install"
-cd /usr/src/app/
-rm -rf /usr/src/app/node_modules
-
-npm install
-
-echo "Moving service code"
-# move our service code to the app folder. In theory we only need the package.json and package-lock.json files
-docker-rsync /data/service/ /usr/src/app/app/
-rm -rf /usr/src/app/app/node_modules
-
-# sync the node modules of template to service directory.
-# do this after we took the service code to not take them with us
-docker-rsync /usr/src/app/node_modules /data/service/
-
-cd /usr/src/app/app/
-
-echo "Running service npm install"
-npm install
-
-# sync the node modules of app to service directory
-docker-rsync /usr/src/app/app/node_modules/ /data/service/node_modules/
-
-cd /usr/src/app/
-
-echo "Generating mu module"
-
-mkdir /usr/src/app/built-mu
-/usr/src/app/node_modules/.bin/babel \
-  /usr/src/app/helpers/mu/ \
-  --source-maps true \
-  --out-dir /usr/src/app/built-mu \
-  --extensions ".js"
-
-cp -R /usr/src/app/built-mu /data/service/node_modules/mu
+docker-rsync /usr/src/app/app/node_modules/ /data/service/node_modules
+cp /usr/src/app/app/package-lock.json /data/service/package-lock.json
