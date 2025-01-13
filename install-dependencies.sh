f0424f756fb420d7286abc8f338c73ed55150647
Aad Versteden <madnificent@gmail.com>
2025-01-13 17:21:10 +0100

    Disable transpilation of current mu sources
    The current node version is sufficient for the way we wrote the code.  The
    sourcemaps weren't generated in a way our language server configuration
    appreciated or babel doesn't generate what it expected.  We can disable for now
    and turn it on again later if new game-changing features arrive.
    
    Co-author: @erikap

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -8,21 +8,4 @@ cd /usr/src/app/app/
 
 npm install
 
-# if processing exists we can take the built mu module out of it directly, we built it in an earlier step
-if [ -d /usr/src/processing/built-mu ]
-then
-  cp -R /usr/src/processing/built-mu /usr/src/app/app/node_modules/mu
-# else we need to create processing npm install and build it again
-else
-  mkdir -p /usr/src/processing/built-mu
-  cd /usr/src/processing/
-  docker-rsync /usr/src/app/. .
-  npm install
-  /usr/src/app/node_modules/.bin/babel \
-    /usr/src/app/helpers/mu/ \
-    --source-maps true \
-    --out-dir /usr/src/processing/built-mu \
-    --extensions ".js"
-
-  cp -R /usr/src/processing/built-mu /usr/src/app/app/node_modules/mu
-fi
+cp -R /usr/src/app/helpers/mu /usr/src/app/app/node_modules/mu
