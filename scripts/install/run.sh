60359575381b937d1f653cd4d46d4ea9fa646011
Aad Versteden <madnificent@gmail.com>
2025-01-13 17:17:00 +0100

    Add commentary to run.sh
    noob comments but they may help a second reading
    
    Co-author: @erikap

--- scripts/install/run.sh
+++ scripts/install/run.sh
@@ -7,10 +7,12 @@ then
     # install script expects service package.json to be in /app
     cp /data/service/package.json /app/package.json
 fi
+# this script runs from a clean slate, the app hasn't ran yet so /usr/src/app/app is empty
 docker-rsync /data/service/ /usr/src/app/app
 cd /usr/src/app
 NODE_ENV=development ./install-dependencies.sh
 
+# copy results into the service's source folder
 docker-rsync /usr/src/app/app/node_modules/ /data/service/node_modules
 if [ -f /usr/src/app/app/package-lock.json ]
 then
