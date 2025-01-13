d267699e501a91dcf30c0b08c1f92f1edb2a6d84
Aad Versteden <madnificent@gmail.com>
2025-01-13 15:27:38 +0100

    Code conventions and questions
    - branching rather than early return
    - positive if clause (as a result of the former)
    - some questions in the code itself
    
    Co-author: @erikap

--- merge-package-json.js
+++ merge-package-json.js
@@ -6,38 +6,37 @@ const OVERRIDE_TEMPLATE_DEPENDENCIES =
 function mergePackageJson() {
   const templatePackage = JSON.parse(fs.readFileSync("package.json", "utf8"));
 
-  if (!fs.existsSync("/app/package.json")) {
+  if (fs.existsSync("/app/package.json")) {
+    const servicePackage = JSON.parse(
+      fs.readFileSync("/app/package.json", "utf8")
+    );
+
+    // QUESTION: What if there is a package.json but the dependencies are undefined (just consuming the versions)
+    warnAboutVersionDifferences(templatePackage, servicePackage);
+
+    if (OVERRIDE_TEMPLATE_DEPENDENCIES) {
+      servicePackage.dependencies = {
+        ...templatePackage.dependencies,
+        ...servicePackage.dependencies,
+      };
+    } else {
+      servicePackage.dependencies = {
+        ...servicePackage.dependencies,
+        ...templatePackage.dependencies,
+      };
+    }
+
+    fs.writeFileSync(
+      "/tmp/package.json",
+      JSON.stringify(servicePackage, null, 2)
+    );
+  } else {
+    // just use the template package.json
     fs.writeFileSync(
       "/tmp/package.json",
       JSON.stringify(templatePackage, null, 2)
     );
-
-    // nothing left to do, just use the template package.json
-    return;
   }
-
-  const servicePackage = JSON.parse(
-    fs.readFileSync("/app/package.json", "utf8")
-  );
-
-  warnAboutVersionDifferences(templatePackage, servicePackage);
-
-  if (OVERRIDE_TEMPLATE_DEPENDENCIES) {
-    servicePackage.dependencies = {
-      ...templatePackage.dependencies,
-      ...servicePackage.dependencies,
-    };
-  } else {
-    servicePackage.dependencies = {
-      ...servicePackage.dependencies,
-      ...templatePackage.dependencies,
-    };
-  }
-
-  fs.writeFileSync(
-    "/tmp/package.json",
-    JSON.stringify(servicePackage, null, 2)
-  );
 }
 
 function warnAboutVersionDifferences(templatePackage, servicePackage) {
@@ -47,6 +46,7 @@ function warnAboutVersionDifferences(templatePackage, servicePackage) {
       templatePackage.dependencies[dep] !== servicePackage.dependencies[dep]
     ) {
       const winner = OVERRIDE_TEMPLATE_DEPENDENCIES ? "service" : "template";
+      // QUESTION: Can we cope with compatible definitions?
       console.warn(
         `Warning: Dependency ${dep} has different versions in template and service package.json. Using ${winner} version.`
       );
