444497ec42cb42b39fc84f266826c3778e6d8e6e
Aad Versteden <madnificent@gmail.com>
2025-01-16 23:36:39 +0100

    Always run installation script for now
    This is not the approach we want to take in the end, but doing so allows to
    debug a bit better what is going on.  We currently expect install-dependencies
    to become a script which consists of two steps which can also independently be
    called from other code paths.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,43 +4,27 @@ source ./helpers.sh
 
 cd /usr/src/app/
 node ./merge-package-json.js
-cp /tmp/merged-package.json{,-bu}
 mv /tmp/merged-package.json /usr/src/app/app/package.json
 cd /usr/src/app/app/
 
 mkdir -p /usr/src/app/app/node_modules/
-# if processing exists we can take the built mu module out of it directly, we built it in an earlier step
-if [ -d /usr/src/processing/built-mu ]
+
+npm install # This should be conditional
+cp -R /usr/src/app/helpers/mu /usr/src/app/app/node_modules/mu
+
+## This code probably belongs elsewhere
+
+## Ensure package.json has module
+cat /usr/src/app/app/package.json | jq -e ".type" > /dev/null
+if [  $? -ne 0 ]
 then
-  cp -R /usr/src/processing/built-mu /usr/src/app/app/node_modules/mu
-# else we need to create processing npm install and build it again
+    echo '[WARNING] Adding "type": "module" to your package.json.'
+    echo 'To remove this warning, add "type": "module" at the same level as "name" in your package.json'
+    sed -i '0,/{/s/{/{\n  "type": "module",/' /usr/src/app/app/package.json
 else
-  npm install
-  mkdir -p /usr/src/processing/built-mu
-  cd /usr/src/processing/
-  docker-rsync /usr/src/app/. .
-  /usr/src/app/node_modules/.bin/babel \
-    /usr/src/app/helpers/mu/ \
-    --source-maps true \
-    --out-dir /usr/src/processing/built-mu \
-    --extensions ".js"
-
-  cp -R /usr/src/processing/built-mu /usr/src/app/app/node_modules/mu
-  cp /usr/src/app/helpers/mu/package.json /usr/src/processing/built-mu/
-
-  ## Ensure package.json has module
-  # cp /app/package.json /usr/src/build/ # already copied above
-  cat /usr/src/app/app/package.json | jq -e ".type" > /dev/null
-  if [  $? -ne 0 ]
-  then
-      echo '[WARNING] Adding "type": "module" to your package.json.'
-      echo 'To remove this warning, add "type": "module" at the same level as "name" in your package.json'
-      sed -i '0,/{/s/{/{\n  "type": "module",/' /usr/src/app/app/package.json
-  else
-      PACKAGE_TYPE=`cat /usr/src/app/app/package.json | jq -r ".type"`
-      if [[ "$PACKAGE_TYPE" -ne "module" ]]
-      then
-          echo '[WARNING] DIFFERENT TYPE THAN "module" IN package.json; CONTINUING WITH UNSPECIFIED BEHAVIOUR'
-      fi
-  fi
+    PACKAGE_TYPE=`cat /usr/src/app/app/package.json | jq -r ".type"`
+    if [[ "$PACKAGE_TYPE" -ne "module" ]]
+    then
+        echo '[WARNING] DIFFERENT TYPE THAN "module" IN package.json; CONTINUING WITH UNSPECIFIED BEHAVIOUR'
+    fi
 fi
