8b52537d19f76da0125ebdcac078bdede1c00039
Oscar <lagartoverde97@gmail.com>
2025-04-11 13:57:30 +0200

    move the start server code outside the mu library

--- transpile-sources.sh
+++ transpile-sources.sh
@@ -85,4 +85,4 @@ docker-rsync /usr/src/app/app/package.json /usr/src/dist/package.json
 # Add server start script
 #########################
 ls /usr/src/app/
-cat /usr/src/app/helpers/mu/start-server.js >> /usr/src/dist/app.js
+cat /usr/src/app/start-server.js >> /usr/src/dist/app.js
