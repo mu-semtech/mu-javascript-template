c4eac735b725c2c30c8b1af283e33458526ffeeb
Erika Pauwels <erika.pauwels@gmail.com>
2025-08-14 12:48:52 +0200

    WIP - quick exit

--- start-server.js
+++ start-server.js
@@ -1,18 +1,23 @@
-import { app, getExitHandler } from 'mu';
+import { app, exitHandler } from 'mu';
 import './app.js'; // load the user's app
 
 var port = process.env.PORT || '80';
 var hostname = process.env.HOST || '0.0.0.0';
 
 // start server
+console.log('yes, it changed');
 const server = app.listen(port, hostname, function () {
   console.log(`Starting server on ${hostname}:${port} in ${app.get('env')} mode`);
 });
 
 // faster stopping
-process.on('SIGTERM', () => {
-  getExitHandler()(server)
+process.on('SIGTERM', async () => {
+  console.log('SIGTERM received');
+  await exitHandler(server);
+  process.exit(0);
 });
-process.on('SIGINT', () => {
-  getExitHandler()(server);
+process.on('SIGINT', async () => {
+  console.log('SIGINT received');
+  await exitHandler(server);
+  process.exit(0);
 });
